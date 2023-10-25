20daf9a0b45b681ac53f105d87e7a079760ea6e6
willbartelt15 <bartelt.will@gmail.com>
2023-10-25 21:07:05 +0000

    Dictionary API

--- api/CalCalc.py
+++ api/CalCalc.py
@@ -1,52 +1,16 @@
 from flask import Flask, request, jsonify
-import sqlite3
 
 app = Flask(__name__)
 
-# Initialize the SQLite database
-conn = sqlite3.connect('data.db')
-cursor = conn.cursor()
+# Initialize an empty dictionary to store user data
+user_data = {}
 
-# Create a table to store the data
-cursor.execute('''CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, value TEXT)''')
-conn.commit()
-
-# Close the database connection when the application is done
-conn.close()
-
-@app.route('/store', methods=['POST'])
-def store_data():
-    try:
-        data = request.get_json()
-        value = data['value']
-
-        conn = sqlite3.connect('data.db')
-        cursor = conn.cursor()
-
-        cursor.execute('INSERT INTO data (value) VALUES (?)', (value,))
-        conn.commit()
-        conn.close()
-
-        return jsonify({'message': 'Data stored successfully'})
-
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-@app.route('/retrieve', methods=['GET'])
-def retrieve_data():
-    try:
-        conn = sqlite3.connect('data.db')
-        cursor = conn.cursor()
-
-        cursor.execute('SELECT * FROM data')
-        data = cursor.fetchall()
-        conn.close()
-
-        data_list = [{'id': item[0], 'value': item[1]} for item in data]
-        return jsonify(data_list)
-
-    except Exception as e:
-        return jsonify({'error': str(e)})
+@app.route('/api/saveUserData', methods=['POST'])
+def save_user_data():
+    data = request.json
+    username = data.get('username')
+    user_data[username] = data  # Store user data in the dictionary
+    return jsonify({"message": "Data saved successfully"})
 
 if __name__ == '__main__':
     app.run(debug=True)

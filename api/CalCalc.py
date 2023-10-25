31e9e76dc23a6f83b8520622f2622aa5a2897f74
willbartelt15 <bartelt.will@gmail.com>
2023-10-25 21:31:41 +0000

    updated backend to store user data

--- api/CalCalc.py
+++ api/CalCalc.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app = Flask(  name)
 
 # Initialize an empty dictionary to store user data
 user_data = {}
@@ -8,8 +8,16 @@ user_data = {}
 @app.route('/api/saveUserData', methods=['POST'])
 def save_user_data():
     data = request.json
-    username = data.get('username')
-    user_data[username] = data  # Store user data in the dictionary
+
+    # Ensure the "username" key is in the data
+    if 'username' not in data:
+        return jsonify({"error": "Username is required."}), 400
+
+    username = data['username']
+
+    # Store user data in the dictionary
+    user_data[username] = data
+
     return jsonify({"message": "Data saved successfully"})
 
 if __name__ == '__main__':
